6ccdc418d2e83754853c559ac93345f41c809739
nehapandit203 <nehapandit203@gmail.com>
2024-05-20 21:33:28 +0530

    Project base

---
+++ src/test/java/lowleveldesignproblems/lowleveldesignproblems/LowLevelDesignProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package lowleveldesignproblems.lowleveldesignproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
